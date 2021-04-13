ec3a4e8b08316d73d6375af653cb9768ff0d7d2f
Firstname Lastname <delfama@whs.school.nz>
2021-04-14 10:51:48 +1200

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Prints hello world.
+ *
+ * @author cantthinkoffname
+ * @version 0
+ */
+
+public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("Hello World");
+    }
+}
